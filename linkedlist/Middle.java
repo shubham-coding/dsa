eeab12daa9f8c5f83171bce3c0d919d05c71ca03
shubham-coding <64828932+shubham-coding@users.noreply.github.com>
2020-05-05 13:02:23 +0530

    refactoring repo

---
+++ linkedlist/Middle.java
@@ -0,0 +1,22 @@
+package linkedlist;
+
+class Node5{
+	int data;
+	Node5 next;
+	
+	public Node5(int data) {
+		this.data = data;
+		this.next = null;		
+	}
+}
+public class Middle {
+	
+	Node5 head;
+	
+	public static void main(String[] a) {
+		
+	}
+	
+
+}
+ 
